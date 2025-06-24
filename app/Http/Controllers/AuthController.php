bd3c40f838e6fd01b43eda134f29f370c49e8233
Enes Gjurgjiali <enesgjurgjiali04@gmail.com>
2025-06-24 13:38:47 +0200

    Implement auth functionality

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        sleep(1);
+        // Validate the request data
+        $fields = $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],  
+            'password' => ['required', 'confirmed']
+        ]);
+
+        // Register the user
+        $user = User::create($fields);  
+
+
+        //Login 
+        Auth::login($user);
+
+        //Redorect 
+        return redirect()->route('home');
+    }
+}
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { reactive } from 'vue';
+import { useForm } from "@inertiajs/vue3";
 
+const form = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+});
 
-  const form = reactive({
-    name: null,
-    email: null,
-    password: null,
-    password_confirmation: null
-  });
-
-  const submit = () => {
-    console.log(form);
-  };
-
+const submit = () => {
+    form.post(route("register"), {
+        onError: () => form.reset("password", "password_confirmation"),
+    });
+};
 </script>
 
 <template>
@@ -20,31 +20,67 @@ import { reactive } from 'vue';
     <h1 class="title">Register a new account</h1>
 
     <div class="w-2/4 mx-auto">
-      <form @submit.prevent="submit">
-        <div class="mb-6">
-          <label>Name</label>
-          <input type="text" v-model="form.name" />
-        </div>
+        <form @submit.prevent="submit">
+            <div class="mb-6">
+                <label>Name</label>
+                <input
+                    type="text"
+                    v-model="form.name"
+                    class="w-full px-4 py-2 border rounded-lg"
+                    :class="{ 'border-red-500': form.errors.name }"
+                />
+                <small class="text-red-500 text-sm mt-1 block">{{
+                    form.errors.name
+                }}</small>
+            </div>
 
-        <div class="mb-6">
-          <label>Email</label>
-          <input type="text" v-model="form.email" />
-        </div>
+            <div class="mb-6">
+                <label>Email</label>
+                <input
+                    type="email"
+                    v-model="form.email"
+                    class="w-full px-4 py-2 border rounded-lg"
+                    :class="{ 'border-red-500': form.errors.email }"
+                />
+                <small class="text-red-500 text-sm mt-1 block">{{
+                    form.errors.email
+                }}</small>
+            </div>
 
-        <div class="mb-6">
-          <label>Password</label>
-          <input type="password" v-model="form.password"/>
-        </div>
+            <div class="mb-6">
+                <label>Password</label>
+                <input
+                    type="password"
+                    v-model="form.password"
+                    class="w-full px-4 py-2 border rounded-lg"
+                    :class="{ 'border-red-500': form.errors.password }"
+                />
+                <small class="text-red-500 text-sm mt-1 block">{{
+                    form.errors.password
+                }}</small>
+            </div>
 
-        <div class="mb-6">
-          <label>Confirm Password</label>
-          <input type="password" v-model="form.password_confirmation"/>
-        </div>
+            <div class="mb-6">
+                <label>Confirm Password</label>
+                <input
+                    type="password"
+                    v-model="form.password_confirmation"
+                    class="w-full px-4 py-2 border rounded-lg"
+                />
+            </div>
 
-        <div class="">
-          <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
-          <button class="primary-btn">Register</button>
-        </div>
-      </form>
+            <div class="">
+                <p class="text-slate-600 mb-2">
+                    Already a user? <a href="#" class="text-link">Login</a>
+                </p>
+                <button
+                    type="submit"
+                    class="primary-btn"
+                    :disabled="form.processing"
+                >
+                    {{ form.processing ? "Registering..." : "Register" }}
+                </button>
+            </div>
+        </form>
     </div>
 </template>

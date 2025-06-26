7f0f5324e4e5b36ca02255c4a5b471ffac514939
Enes Gjurgjiali <enesgjurgjiali04@gmail.com>
2025-06-26 08:24:48 +0200

    Implement user search functionality and enhance dashboard greeting message

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,22 @@
 <script setup>
-import PaginationLinks from './Components/PaginationLinks.vue';
-defineProps({
+import { ref, watch } from "vue";
+import { router } from "@inertiajs/vue3";
+import PaginationLinks from "./Components/PaginationLinks.vue";
+import { debounce } from "lodash";
+
+const props = defineProps({
     users: Object,
+    searchTerm: String,
+    can: Object,
 });
+
+const search = ref(props.search);
+
+// Watch for search input changes, (debounce to prevent too many requests) or (throttle)
+watch(search, debounce((q) => router.get('/', {search: q}, { preserveState: true}),
+    500)
+);
+    
 
 // Format Date
 const getDate = (date) => {
@@ -17,7 +31,16 @@ const getDate = (date) => {
 <template>
     <Head :title="` | ${$page.component} `" />
 
-    <div class="">
+    <div>
+        <div class="flex justify-end mb-4">
+            <div class="w-1/4">
+                <input
+                    type="search"
+                    placeholder="Search users..."
+                    v-model="search"
+                />
+            </div>
+        </div>
         <table>
             <thead>
                 <tr class="bg-slate-300">
@@ -25,6 +48,7 @@ const getDate = (date) => {
                     <th>Name</th>
                     <th>Email</th>
                     <th>Registration Date</th>
+                    <th v-if="can.delete_user">Delete</th>
                 </tr>
             </thead>
 
@@ -43,6 +67,9 @@ const getDate = (date) => {
                     <td>{{ user.name }}</td>
                     <td>{{ user.email }}</td>
                     <td>{{ getDate(user.created_at) }}</td>
+                    <td v-if="can.delete_user">
+                        <button class="bg-red-500 w-6 h-6 rounded-full"></button>
+                    </td>
                 </tr>
             </tbody>
         </table>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,30 @@
 
 use App\Http\Controllers\AuthController;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Home Route
-Route::inertia('/', 'Home', ['users' => User::paginate(5)])->name('home');
+Route::get('/', function(Request $request){
+  return inertia('Home', [
+    'users' => User::when($request->search, function($query) use ($request) {
+      $query
+      ->where('name', 'like', '%' . $request->search . '%')
+      ->orWhere('email', 'like', '%' . $request->search . '%');
+    }) -> paginate(5)->withQueryString(),
+
+    'searchTerm' => $request->search,
+
+    //If the user is authenticated, they can delete a user
+    'can' => [
+      'delete_user' => 
+            Auth::user() ? 
+                  Auth::user()->can('delete', User::class) : 
+                  null
+    ]
+  ]);
+})->name('home');
 
 //Middleware for authenticated users
   Route::middleware(['auth'])->group(function () {
